d1e0f0812b95e0a34741a13de7afaf2412e7e3b4
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-12-22 12:14:30 +0400

    faced some error since i forget to do migration and database update

--- Migrations/20241222081237_Cart.Designer.cs
+++ Migrations/20241222081237_Cart.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrderMnagementAPIs;
 
@@ -11,9 +12,11 @@ using OrderMnagementAPIs;
 namespace OrderMnagementAPIs.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241222081237_Cart")]
+    partial class Cart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
